a9c3c56525823302c54747f05d266adfa0b5a725
Cedric Champeau <cedric.champeau@gmail.com>
2021-02-16 23:36:14 +0100

    Introduce a platform module

--- jdoctor-core/build.gradle.kts
+++ jdoctor-core/build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     groovy
-    `maven-publish`
     id("me.champeau.java-module")
+    id("me.champeau.publishing")
 }
 
-group = "me.champeau.jdoctor"
-version = "0.1-SNAPSHOT"
-
 dependencies {
+    api(platform(project(":jdoctor-bom")))
     testImplementation("org.codehaus.groovy:groovy:2.5.13")
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
     testImplementation("junit:junit:4.13")
@@ -15,16 +13,6 @@ dependencies {
     testFixturesImplementation("org.codehaus.groovy:groovy:2.5.13")
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "projectLocal"
-            url = uri("${buildDir}/repo")
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
-}
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,9 @@ rootProject.name = "jdoctor"
 
 includeBuild("build-logic")
 
+include("jdoctor-bom")
 include("jdoctor-core")
+
 
 dependencyResolutionManagement {
     repositories {
